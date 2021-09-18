3486aaa9634c9923885d31629a64ec6756c6bc53
Farhan <farhan@iosentrix.com>
2021-09-18 17:36:16 -0400

    update

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask import jsonify, request
 import auth, utils, json
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
 @app.route("/api/login", methods = ['POST'])
+@cross_origin(origin='*',headers=['Content-Type','token'])
 def login():
     return auth.login(request.get_json(force=True))
 
 
 @app.route("/api/signup", methods = ['POST'])
+@cross_origin(origin='*',headers=['Content-Type','token'])
 def signup():
 	data = request.get_json(force=True)
 	data=json.dumps(data)
@@ -29,6 +32,7 @@ def signup():
 
 
 @app.route("/api/search", methods = ['POST'])
+@cross_origin(origin='*',headers=['Content-Type','token'])
 def search():
 	token=request.headers.get('token')
 	if token:
@@ -40,3 +44,20 @@ def search():
 			return jsonify({"msg":"Please login"})
 	else:
 		return jsonify({"msg":"Please login"})
+
+
+@app.route("/api/message/send", methods = ['POST'])
+@cross_origin(origin='*',headers=['Content-Type','token'])
+def send():
+	token=request.headers.get('token')
+	if token:
+		if(auth.check_auth(token)):
+			data=request.get_json(force=True)
+			return utils.send(data)
+		else:
+			return jsonify({"msg":"Please login"})
+	else:
+		return jsonify({"msg":"Please login"})
+
+
+
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
-import re
+import re, base64
 from config import get_config
 from flask import jsonify, request
+from datetime import datetime
 
 redis_client = get_config().SESSION_REDIS
 
@@ -28,3 +29,17 @@ def search(query):
 				pass
 
 	return jsonify({"users":users})
+
+def send(data):
+	receiver_id=data["r_id"]
+	token=request.headers.get('token')
+	sender_id=str(token).split(':')[-1]
+	send_time=datetime.now()
+	msg=data["msg"]
+	next_id = redis_client.incr("total_msgs")
+	msg_key = f"msg:{next_id}"
+	redis_client.hmset(msg_key, {"sender": sender_id, "receiver": receiver_id, "time":str(send_time), 'msg':str(base64.b64encode(bytes(msg, 'utf-8')))})
+
+	return jsonify({"msg":"message sent","status":"ok"})
+
+
